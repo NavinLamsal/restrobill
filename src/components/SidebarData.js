07429eed8d9495bb0807ef1bafff9b8d993e3772
Navin lamsal <navinlamsal11@gmail.com>
2022-12-09 14:34:22 +0545

    1st

---
+++ src/components/SidebarData.js
@@ -0,0 +1,37 @@
+import React from "react";
+import '../App.css';
+
+
+export const SidebarData = [
+    {
+        title:"home",
+        icon: <icon/>,
+        link:"/home"
+    },
+    {
+        title:"Dashboard",
+        icon: <icon/>,
+        link:"/home"
+    },
+    {
+        title:"Table",
+        icon: <icon/>,
+        link:"/Table"
+    },
+    {
+        title:"Order",
+        icon: <icon/>,
+        link:"/Order"
+    },
+    {
+        title:"Bill",
+        icon: <icon/>,
+        link:"/Bill"
+    },
+    {
+        title:"Payment",
+        icon: <icon/>,
+        link:"/Payment"
+    }
+]
+
